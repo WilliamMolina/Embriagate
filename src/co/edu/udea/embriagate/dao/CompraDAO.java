e927bb01761e32ec49353a4e30814d0ca9394f66
william.molina <wmolina98@gmail.com>
2015-12-17 20:03:50 -0500

    DAOImpl terminados

---
+++ src/co/edu/udea/embriagate/dao/CompraDAO.java
@@ -0,0 +1,27 @@
+package co.edu.udea.embriagate.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import co.edu.udea.embriagate.dto.CompraDTO;
+import co.edu.udea.embriagate.dto.UsuarioDTO;
+import co.edu.udea.embriagate.exception.MyException;
+
+//Interfaz para Compra
+public interface CompraDAO {
+	
+	//M�todo para crear una compra
+	public void crearCompra(CompraDTO compra) throws MyException;
+	
+	//M�todo para obtener todas las compras
+	public List<CompraDTO> obtenerTodasLasCompras() throws MyException;
+	
+	//Metodo para obtener las compras de un usuario en especifico
+	public List<CompraDTO> obtenerComprasPorUsuario(UsuarioDTO usuario) throws MyException;
+	
+	//M�todo para obtener las compras en un rango de fechas
+	public List<CompraDTO> obtenerComprasPorRangoFecha(Date desde, Date hasta) throws MyException;
+	//Metodo para obtener una compra dado su numero
+	public CompraDTO obtenerCompraPorNumero(Long numero) throws MyException;
+
+}
